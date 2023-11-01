0f2fbd09babc29c2cc4b2d6a9d9f4b10dcdeb5ec
Prapanchj <145548350+Prapanchj@users.noreply.github.com>
2023-11-01 15:24:12 +0530

    Add files via upload

---
+++ arra.java
@@ -0,0 +1,28 @@
+package prj;
+
+import java.util.Scanner;
+
+public class arra {
+	public static void main(String[]args) {
+Scanner sc=new Scanner (System.in);
+		int i,j,lim,tem;
+		System.out.println("enter the limit");
+lim=sc.nextInt();
+int array[]=new array();
+
+for(i=0;i<lim;i++) {
+array[i]=sc.nextInt();	
+}
+	for(i=0;i<lim;i++) {
+	for(j=0;j<lim;j++) {
+		if(array[j]>array[j++]) {
+	
+		tem=array[j];
+				array[j]=tem;
+				array[j+1]=tem;
+	}
+	}
+	}
+for(int element:array) 
+
+}
